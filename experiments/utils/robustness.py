9e8f50b18f7c1223baa857e4e19a4bc09eb6a558
Craig Pirie <c.pirie11@rgu.ac.uk>
2024-02-07 16:27:30 +0000

    added gower distance to robustness

--- experiments/utils/robustness.py
+++ experiments/utils/robustness.py
@@ -3,21 +3,35 @@ from sklearn.neighbors import NearestNeighbors
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 from scipy.stats import spearmanr
+import math
 
 
 class Robustness:
     def __init__(self):
         self.test = ""
 
-    def linear_interpolate(self, query_sample, farthest_neighbor, num_steps=50):
-        interpolated_samples = np.zeros((num_steps, query_sample.shape[1]))
+    def linear_interpolate(self, query_sample, farthest_neighbor, num_steps=30, categorical_columns=[]):
+        # Initialize an array to hold the interpolated samples
+        interpolated_samples = np.zeros((num_steps, len(query_sample)))
+
+        # Handle numerical interpolation
         for step in range(num_steps):
             t = step / float(num_steps - 1)
-            interpolated_samples[step] = query_sample + t * (farthest_neighbor - query_sample)
+            interpolated_samples[step, :] = query_sample + t * (farthest_neighbor - query_sample)
+
+        # Handle categorical columns
+        for cat_col in categorical_columns:
+            midpoint = num_steps // 2
+            for step in range(num_steps):
+                if step < midpoint:
+                    interpolated_samples[step, cat_col] = query_sample[cat_col]
+                else:
+                    interpolated_samples[step, cat_col] = farthest_neighbor[cat_col]
+
         return interpolated_samples
     
     
-    def generate_perturbations(self, data_to_explain, model, X):
+    def generate_perturbations(self, data_to_explain, model, X, num_perturbations = 30, categorical_columns = []):
         perturbed_instances = []
 
         # Predict classes for the entire dataset
@@ -41,7 +55,7 @@ class Robustness:
             nun_instance = unlike_data[nun_idx]
 
             # Generate linear perturbations between the instance to explain and its nearest unlike neighbor
-            perturbations = self.linear_interpolate(dte.reshape(1, -1), nun_instance.reshape(1, -1), 30)
+            perturbations = self.linear_interpolate(dte, nun_instance, num_perturbations, categorical_columns)
             perturbed_instances.append(perturbations)
 
         return perturbed_instances
@@ -56,55 +70,22 @@ class Robustness:
         
         return ndisordered / (n * (n - 1))
 
-    def calculate_robustness(self, original_explanation, perturbed_explanations, distance_method="euclidean", weight=0.7, show=None):
-        calculate_distance = distance.euclidean
-
-        if(distance_method == "cosine"):
-            calculate_distance = distance.cosine
-        elif(distance_method == "kendall"):
-            calculate_distance = self.kendall_tau_distance
+    def calculate_robustness(self, original_explanation, perturbed_explanations, r=2, categorical_columns = [], ranges = [], show=None):
 
         distances = []
         for perturbed_explanation in perturbed_explanations:
-            explanation_distance = calculate_distance(original_explanation, perturbed_explanation)
+            explanation_distance = self.calculate_gower_distance(original_explanation, perturbed_explanation, r, categorical_columns, ranges)
             distances.append(explanation_distance)
-
 
         distances = np.array(distances)
 
-        if(weight > 0):
-            # Calculate the weights using exponential decay based on the index
-            weights = [np.exp(-weight * (i + 1)) for i in range(len(perturbed_explanations))]
-            # Calculate the sum of the unnormalized weights
-            sum_weights = sum(weights)
-
-            # Normalize the weights
-            normalized_weights = [w / sum_weights for w in weights]
-
-            distances = np.array([distances[i] * normalized_weights[i] for i in range(len(distances))])
-
-        # Calculate the minimum and maximum distances
-        min_distance = np.min(distances)
-        max_distance = np.max(distances)
-
-        # Normalize the distances
-        normalized_distances = []
-        for d in distances: 
-            if(max_distance > min_distance):
-                normalized_distance = (d - min_distance) / (max_distance - min_distance)
-            else:
-                normalized_distance = 0
-            normalized_distances.append(normalized_distance)
-
-        normalized_distances = np.array(normalized_distances)
-
         # Calculate the similarity using 1 - normalized distance
-        similarities = 1 - normalized_distances
+        similarities = 1 - distances
 
         if(show == "similarity"):
             self.plot_robustness_curve(list(range(1,len(similarities)+1)), similarities, True)
         elif(show == "distance"):
-            self.plot_robustness_curve(list(range(1,len(normalized_distances)+1)), normalized_distances, False)
+            self.plot_robustness_curve(list(range(1,len(distances)+1)), distances, False)
         
         # Create an array of x-values
         x_values = np.linspace(0, 1, len(similarities))
@@ -132,10 +113,23 @@ class Robustness:
     def calculate_sensitivity(self, original_explanation, perturbed_explanations):
         max_difference = 0
         for perturbation in perturbed_explanations:
-            distance = distance.euclidean(original_explanation, perturbation)
-            max_difference = max(max_difference, distance)
+            d = distance.euclidean(original_explanation, perturbation)
+            max_difference = max(max_difference, d)
         
         return max_difference
+    
+    def calculate_gower_distance(self, query, perturbation, r, categorical_columns = [], ranges = []):
+
+        distance = 0
+        for i, feature in enumerate(query):
+
+            if(i in categorical_columns):
+                if(feature != perturbation[i]):
+                    distance += 1
+            else:
+                numeric_distance = pow(abs(feature - perturbation[i]), r) / ranges[i]
+                distance += numeric_distance
+        return distance / len(query)
 
 
         
